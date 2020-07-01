88bccd794e70998be84b756b4437c418c48d1ce8
bbnam <bbnam1591999@gmail.com>
2020-07-01 12:36:17 +0700

    searchBar

--- app/components/SearchBar.js
+++ app/components/SearchBar.js
@@ -1,24 +1,47 @@
 import React from 'react';
 import { Icon } from 'react-native-elements';
-import { View, TextInput, Dimensions, TouchableOpacity, Image } from 'react-native'
+import { View, TextInput, Dimensions, TouchableOpacity, Image, Text, ScrollView, TouchableHighlight } from 'react-native'
 import styles from './ComponentStyles/SearchBar';
-
+import { playlist } from '../data/data';
 const screenHeight = Dimensions.get('window').height;
+import ListView from "deprecated-react-native-listview";
+
+var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 
 export default class SearchBar extends React.Component {
   constructor(props){
     super(props)
     this.state={
-      searchText: ''
+      searchedSong: []
     }
   }
-
+  _pressRow = () => {
+    console.log("clicked")
+  }
   updateSearch = searchText => {
     this.setState({searchText})
   }
+  searchedSong = (searchedText) => {
+    var searchedSong = playlist.filter(function(playlist) {
+      return playlist.title.toLowerCase().indexOf(searchedText.toLowerCase()) > -1;
+    });
+    this.setState({searchedSong: searchedSong});
+  };
 
+    renderSong = (playlist) => {
+    return (
+      <View>
+      <TouchableHighlight onPress={() => this._pressRow()}>
+        <View>
+          <Text>{playlist.title}, {playlist.artist}</Text>
+        </View>
+      </TouchableHighlight>
+      </View>
+    );
+  };
   render(){
     const {searchText} = this.state;
+    console.log(this.state.searchedSong)
     return(
       <View style={styles.container}>
         <View style={[styles.searchBarContainer, this.props.style]}>
@@ -26,9 +49,17 @@ export default class SearchBar extends React.Component {
             <TextInput
               placeholder="Nhập tên bài hát"
               style={styles.searchInput}
-              onChangeText={searchText => this.updateSearch(searchText)}
-              value={searchText}
-            />   
+              onChangeText={this.searchedSong}
+              // value={searchText}
+            /> 
+              <ScrollView scrollsToTop={false}>
+              <ListView
+                    dataSource={ds.cloneWithRows(this.state.searchedSong)}
+                    renderRow={this.renderSong} 
+                    enableEmptySections={true}/>
+            </ScrollView>
+  
+            
         </View>
         <TouchableOpacity onPress={() => this.props.navigation.navigate("Profile")}>
           <View>
